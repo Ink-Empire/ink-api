f61578a447d641c82bce4900fcda3be91e787ff1
Caroline Ennis <37984545+ccennis@users.noreply.github.com>
2024-04-07 15:40:15 -0400

    initial work to plumb in elasticsearch

---
+++ app/Jobs/ElasticRebuildJob.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Jobs;
+
+use App\Services\ElasticService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Log;
+
+
+class ElasticRebuildJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $ids;
+    public $model;
+
+    public function __construct($model, array $ids = [])
+    {
+        $this->ids = $ids;
+        $this->model = $model;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(ElasticService $elasticService): void
+    {
+        if (!empty($this->ids)) {
+            $elasticService->rebuild($this->ids, $this->model);
+        }
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * @param $exception
+     * @return void
+     */
+    public function failed($exception)
+    {
+        Log::error("JOB LOG job failed to rebuild elastic ids", [
+            'ids' => (array) $this->ids,
+            'model' => $this->model,
+        ]);
+    }
+
+
+    /**
+     * Calculate the number of seconds to wait before retrying the job.
+     *
+     * @return array
+     */
+    public function backoff()
+    {
+        return [10, 20, 45];
+    }
+}
