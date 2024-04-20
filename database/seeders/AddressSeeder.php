36b16f348f3579a0f5e694339352b195122a2b56
Trevor Lemeron <trevorrl@me.com>
2024-04-20 16:16:58 -0400

    add addresses seed data

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Address;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use File;
 
 class AddressSeeder extends Seeder
 {
@@ -14,8 +17,22 @@ class AddressSeeder extends Seeder
      */
     public function run()
      {
-         Address::factory()->generated()
-             ->count(50)
-             ->create();
+         $json = File::get("database/seed-data/addresses.json");
+         $addresses = json_decode($json);
+
+         foreach ($addresses as $key => $value) {
+            Address::create([
+                "first_name" => $value->first_name,
+                "last_name" => $value->last_name,
+                "address1" => $value->address1,
+                "address2" => $value->address2,
+                "city" => $value->city,
+                "state" => $value->state,
+                "postal_code" => $value->postal_code,
+                "country_code" => "US",
+                "phone" => $value->phone,
+                "is_active" => 1
+            ]);
+         }
      }
 }
