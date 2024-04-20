8c2e03afade1625a046dcb54bb8dbda3e04ce0af
Trevor Lemeron <trevorrl@me.com>
2024-04-20 17:31:50 -0400

    add studios seed data

--- database/seeders/StudioSeeder.php
+++ database/seeders/StudioSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Studio;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use File;
 
 class StudioSeeder extends Seeder
 {
@@ -14,9 +17,18 @@ class StudioSeeder extends Seeder
      */
     public function run()
     {
-        Studio::factory()
-            ->count(50)
-            ->hasAddress(1)
-            ->create();
+        $json = File::get("database/seed-data/studios.json");
+        $studios = json_decode($json);
+
+        foreach ($studios as $key => $value) {
+            Studio::create([
+                "name" => $value->name,
+                "email" => $value->email,
+                "phone" => $value->phone,
+                "about" => $value->about,
+                "location" => $value->location,
+                "address_id" => $value->address_id
+            ]);
+        }
     }
 }
