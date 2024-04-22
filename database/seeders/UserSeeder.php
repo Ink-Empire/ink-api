0aa17b40c178bdb44d60d7077658f598a3a83bfb
Trevor Lemeron <trevorrl@me.com>
2024-04-22 15:06:44 -0400

    update format of users seed data and added lat_long field

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,6 +28,7 @@ class UserSeeder extends Seeder
                     "about" => $value->about,
                     "email" => $value->email,
                     "location" => $value->location,
+                    "location_lat_long" => $value->location_lat_long,
                     "name" => $value->name,
                     "studio_id" => $value->studio_id,
                     "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
