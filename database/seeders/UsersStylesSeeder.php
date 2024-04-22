a38046e7737b38a7a142498d675c9767cfa35054
Trevor Lemeron <trevorrl@me.com>
2024-04-22 12:52:27 -0400

    some updates to user styles seed data

--- database/seeders/UsersStylesSeeder.php
+++ database/seeders/UsersStylesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use File;
 
 class UsersStylesSeeder extends Seeder
 {
