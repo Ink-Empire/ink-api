7ca9c8436d2f2eb6a4cd89774c04360c5f5edf98
Caroline Ennis <37984545+ccennis@users.noreply.github.com>
2024-05-02 14:56:57 -0400

    updates to create tattoo process

--- database/migrations/2023_06_25_154448_tattoos.php
+++ database/migrations/2023_06_25_154448_tattoos.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('description');
             $table->string('placement')->nullable();
             $table->foreignId('artist_id')->constrained('users', 'id')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('studio_id')->constrained()->onDelete('no action')->onUpdate('no action');
+            $table->foreignId('studio_id')->constrained()->onDelete('no action')->onUpdate('no action')->nullable();
             $table->foreignId('primary_style_id')->constrained('styles', 'id')->onDelete('no action')->onUpdate('no action');
             $table->foreignId('primary_image_id')->constrained('images', 'id')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
