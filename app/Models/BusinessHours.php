ffdfacc3bc1a44d402cab80aa2f31a846cb859bf
Caroline Ennis <37984545+ccennis@users.noreply.github.com>
2024-05-08 16:41:25 -0400

    business hours logic -- upsert

--- app/Models/BusinessHours.php
+++ app/Models/BusinessHours.php
@@ -9,12 +9,25 @@ class BusinessHours extends Model
 {
     use HasFactory;
 
+    public $appends = ['day'];
+    public $without = ['business_days'];
+
     protected $fillable = [
-        'day',
+        'day_id',
         'open_time',
         'close_time',
         'studio_id'
     ];
+
+    public function getDayAttribute()
+    {
+        return $this->business_days->day;
+    }
+
+    public function business_days()
+    {
+        return $this->belongsTo(BusinessDay::class, 'day_id', 'id');
+    }
 
     public function studio()
     {
