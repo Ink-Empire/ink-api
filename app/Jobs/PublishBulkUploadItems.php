80dcdf3ca2b4dfca2975103ab2837321c127956c
Caroline Ennis <caroline@groupthink.com>
2026-01-11 09:51:35 +1300

    some surgery to queues/jobs

--- app/Jobs/PublishBulkUploadItems.php
+++ app/Jobs/PublishBulkUploadItems.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Enums\QueueNames;
 use App\Models\BulkUpload;
 use App\Models\BulkUploadItem;
 use App\Models\Tattoo;
@@ -20,6 +21,7 @@ class PublishBulkUploadItems implements ShouldQueue
 
     public int $tries = 3;
     public int $timeout = 600; // 10 minutes
+    public $queue = QueueNames::BULK_UPLOAD;
 
     public function __construct(
         public int $bulkUploadId
