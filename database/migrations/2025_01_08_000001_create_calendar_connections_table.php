d407cacb3ab0e7c558e55b0f4bf7cdc6f43f9a23
Caroline Ennis <caroline@groupthink.com>
2026-02-01 15:46:15 +1300

    reverting changes to migrations

--- database/migrations/2025_01_08_000001_create_calendar_connections_table.php
+++ database/migrations/2025_01_08_000001_create_calendar_connections_table.php
@@ -8,10 +8,6 @@ return new class extends Migration
 {
     public function up(): void
     {
-        if (Schema::hasTable('calendar_connections')) {
-            return;
-        }
-
         Schema::create('calendar_connections', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
