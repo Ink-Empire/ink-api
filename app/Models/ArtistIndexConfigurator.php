346287d1be7350e112c58bc22db0d8e5127f8c0e
Caroline Ennis <caroline@groupthink.com>
2026-02-15 14:31:37 +1300

    elastic updates

--- app/Models/ArtistIndexConfigurator.php
+++ app/Models/ArtistIndexConfigurator.php
@@ -20,6 +20,7 @@ class ArtistIndexConfigurator extends IndexConfigurator
     /** @var array */
     protected $settings = [
         "number_of_shards" => 1,
+        "number_of_replicas" => 0,
         'analysis' => [
             'analyzer' => [
                 'search_text_analyzer' => [
